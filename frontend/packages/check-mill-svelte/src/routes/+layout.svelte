4b1eb75fe908ba7816e4cba31fe1f68c519b6287
fliseno1k <fliseno1k@gmail.com>
2025-07-12 21:31:49 +0300

    feat: svelte init

---
+++ frontend/packages/check-mill-svelte/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+	import '../styles/index.scss';
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+	/>
+</svelte:head>
+
+<Header />
+<slot />
